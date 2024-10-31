c1998074ee25397d7d16da9042cb7bac8c3ce2a1
ShadowStar <98736da2cf2fdeb33952364e3ca2a422@firemail.com.br>
2024-10-31 13:33:35 -0700

    Verificando o .env

--- server.js
+++ server.js
@@ -12,6 +12,34 @@ const AutomationService = require('./src/services/AutomationService');
 
 const app = express();
 
+// Verifica se as variaveis do .env foram definidas corretamente
+
+if (!process.env.DB_HOST) {
+    console.error('DB_HOST não está definido.');
+} else if (!process.env.DB_USER) {
+    console.error('DB_USER não está definido.');
+} else if (!process.env.DB_PASS) {
+    console.error('DB_PASS não está definido.');
+} else if (!process.env.DB_NAME) {
+    console.error('DB_NAME não está definido.');
+} else if (!process.env.JWT_SECRET) {
+    console.error('JWT_SECRET não está definido.');
+} else if (!process.env.PORT) {
+    console.error('PORT não está definido.');
+} else if (!process.env.EMAIL_HOST) {
+    console.error('EMAIL_HOST não está definido.');
+} else if (!process.env.EMAIL_PORT) {
+    console.error('EMAIL_PORT não está definido.');
+} else if (!process.env.EMAIL_USER) {
+    console.error('EMAIL_USER não está definido.');
+} else if (!process.env.EMAIL_PASS) {
+    console.error('EMAIL_PASS não está definido.');
+} else if (!process.env.EMAIL_FROM) {
+    console.error('EMAIL_FROM não está definido.');
+} else if (!process.env.EMAIL_SECURE) {
+    console.error('EMAIL_SECURE não está definido.');
+};
+
 // Middlewares
 app.use(helmet());
 app.use(cors());
